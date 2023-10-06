3afc9cb57a6bea069f25fe8165d1eefa62fe13dd
Yanis ♨️ <azellaxmc@gmail.com>
2023-10-06 12:23:07 +0200

    Added ServoManager.h and ServoManager.cpp files
    These files contain the ServoManager class, which provides functionality for managing multiple servos on an Arduino. The class includes methods for attaching servos to pins, setting angles, reading angles, and various servo control functions.
    
    - ServoManager.h: Header file for the ServoManager class.
    - ServoManager.cpp: Implementation file for the ServoManager class.

---
+++ lib/ServoManager/ServoManager.cpp
@@ -0,0 +1,37 @@
+#include "ServoManager.h"
+
+ServoManager::ServoManager()
+{
+    numServos = 0;
+}
+
+void ServoManager::initialize()
+{
+}
+
+void ServoManager::attachServo(int servoIndex, int pin)
+{
+    if (servoIndex < MAX_NUM_SERVOS && numServos < MAX_NUM_SERVOS)
+    {
+        servos[servoIndex].attach(pin);
+        servoPins[servoIndex] = pin;
+        numServos++;
+    }
+}
+
+void ServoManager::setAngle(int servoIndex, int angle)
+{
+    if (servoIndex < numServos)
+    {
+        servos[servoIndex].write(angle);
+    }
+}
+
+int ServoManager::readAngle(int servoIndex)
+{
+    if (servoIndex < numServos)
+    {
+        return servos[servoIndex].read();
+    }
+    return -1; // Valeur d'erreur
+}
---
+++ lib/ServoManager/ServoManager.h
@@ -0,0 +1,36 @@
+#ifndef __ServoManager_h__
+#define __ServoManager_h__
+
+#include "Arduino.h"
+#include "../Servo-1.2.1/src/Servo.h"
+
+#define MAX_NUM_SERVOS 2 // Maximum number of servos
+
+class ServoManager
+{
+public:
+    ServoManager();
+    void initialize();                                        // Initialize the ServoManager
+    void attachServo(int servoIndex, int pin);                // Attach a servo to a pin
+    void setAngle(int servoIndex, int angle);                 // Set the angle of a servo
+    int readAngle(int servoIndex);                            // Read the current angle of a servo
+    void setMinAngle(int servoIndex, int minAngle);           // Set the minimum allowed angle of a servo
+    void setMaxAngle(int servoIndex, int maxAngle);           // Set the maximum allowed angle of a servo
+    void attachAll();                                         // Attach all servos
+    void detachAll();                                         // Detach all servos
+    void writeMicroseconds(int servoIndex, int microseconds); // Write a pulse in microseconds for a servo
+    void write(int servoIndex, int value);                    // Write a value in degrees (0-180) for a servo
+    void showValues();                                        // Show current values of all servos
+    byte setValues();                                         // Implement logic to set servo values
+    void setSpeed(int servoIndex, int speed);                 // Set the speed of a servo
+    void setAcceleration(int servoIndex, int acceleration);   // Set the acceleration of a servo
+    void setSpeeds(int speed);                                // Set the speed for all servos
+    void setAccelerations(int acceleration);                  // Set the acceleration for all servos
+
+private:
+    Servo servos[MAX_NUM_SERVOS];  // Array of servos
+    int servoPins[MAX_NUM_SERVOS]; // Array of pins corresponding to servos
+    int numServos;                 // Current number of attached servos
+};
+
+#endif
