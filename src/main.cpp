90b69929922cc73890085add3adcf069079303c7
Yanis ♨️ <azellaxmc@gmail.com>
2023-10-12 19:11:43 +0200

    Addition of some functionnalities

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,70 @@
 #include <Arduino.h>
+#include <Servo.h>
+#include <Sensors.h>
+#include <ServoManager.h>
 
 #define SERIAL_BAUD 9600
+#define BUTTON_PIN 2
+
+#define LIGHT_SENSOR_PIN A0
+#define LIGHT_SENSOR_THRESHOLD 500
+
+int lastButtonState = LOW;
+int currentButtonState = LOW;
+
+unsigned long buttonPressedMs = millis();
+bool buttonPressed = false;
+bool checkStartPressedButton = true;
+
+Sensor lightSensor(LIGHT_SENSOR_PIN);
+
+void checkButton()
+{
+
+  if ((millis() - buttonPressedMs) > (2 * 1000) && buttonPressed)
+  {
+    Serial.println("Button pressed for 2 seconds");
+    buttonPressed = false;
+  }
+}
+
+void checkBrightness()
+{
+  // Lisez la valeur du capteur de lumière
+  int sensorValue = lightSensor.getSensorValue();
+
+  if (sensorValue < LIGHT_SENSOR_THRESHOLD)
+  {
+    // closeHand();
+    digitalWrite(9, HIGH);
+    delay(1000);
+  }
+  else
+  {
+    // Sinon, ouvrez la main
+    // openHand();
+  }
+}
 
 void setup()
 {
   Serial.begin(SERIAL_BAUD);
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  currentButtonState = digitalRead(BUTTON_PIN);
+
+  lightSensor.initialize(); // Initialisez le capteur de lumière
 }
 
 void loop()
 {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
+
+  checkBrightness();
+
+  lastButtonState = currentButtonState;
+  currentButtonState = digitalRead(BUTTON_PIN);
+
+  if (lastButtonState == HIGH && currentButtonState == LOW)
+  {
+    // Code à exécuter lors de l'appui sur le bouton.
+  }
 }
